3c48ba70470056f8dcb921025833a97c3c098a13
rayanenini <rayane.nini@cpe.fr>
2023-12-05 00:35:24 +0100

    chat

--- backendNodeJs/server.js
+++ backendNodeJs/server.js
@@ -1,92 +1,104 @@
-const app = require('express');
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {cors: {origin: "*"}});
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 const request = require('request');
-
 
 let users = [];
 
 const connectedUsers = {};
 const messageHistory = {};
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toLocaleTimeString()} - ${req.method} request for ${req.url}`);
+  next();
+});
+
 function isValidUser(username, password) {
-    return users.some(user => user.username === username && user.password === password);
+  return users.some(user => user.username === username && user.password === password);
 }
 
 function getUsersFromApi(callback) {
-    const apiUrl = 'http://localhost:8083/users';
+  const apiUrl = 'http://localhost:8083/users';
 
-    request(apiUrl, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            callback(JSON.parse(body));
-        } else {
-            console.error('Erreur lors de la récupération des utilisateurs:', error);
-            callback([]);
-        }
-    });
+  request(apiUrl, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      callback(JSON.parse(body));
+    } else {
+      console.error('Erreur lors de la récupération des utilisateurs:', error);
+      callback([]);
+    }
+  });
 }
 
 async function updateUsers() {
-    getUsersFromApi(apiUsers => {
-        users = apiUsers;
-    });
+  getUsersFromApi(apiUsers => {
+    users = apiUsers;
+  });
 }
 
-
 io.on('connection', (socket) => {
-    console.log('Un utilisateur s\'est connecté');
+  console.log('Un utilisateur s\'est connecté');
 
-    socket.on('login', async (credentials) => {
-        const { username, password } = credentials;
-        await updateUsers();
+  socket.on('login', async (credentials) => {
+    const { username, password } = credentials;
+    await updateUsers();
 
-        if (isValidUser(username, password)) {
-            connectedUsers[socket.id] = { id: users.find(user => user.username === username).id, username };
-            io.to(socket.id).emit('login-success', { id: connectedUsers[socket.id].id, username });
-            io.emit('update-user-list', Object.values(connectedUsers));
+    if (isValidUser(username, password)) {
+      userco = users.find(user => user.username === username);
+      connectedUsers[userco.id] = { id: userco.id, username: userco.username, socket: socket.id };
+      io.to(socket.id).emit('login-success', { id: connectedUsers[userco.id].id, username });
+      io.emit('update-user-list', Object.values(connectedUsers));
 
-            socket.join(`user-${connectedUsers[socket.id].id}`);
-        } else {
-            io.to(socket.id).emit('login-fail');
-        }
-    });
+      socket.join(`user-${connectedUsers[userco.id].id}`);
+    } else {
+      io.to(socket.id).emit('login-fail');
+    }
+  });
 
-    socket.on('message', (message) => {
-        const senderId = connectedUsers[socket.id].id;
+  socket.on('message', (message) => {
+    if(message.receiverId==-1){
+        const senderId = message.id;
+        const timestamp = new Date().toLocaleTimeString();
+        const formattedMessage = `${timestamp} - ${connectedUsers[message.id].username} à tous : ${message.text}`;
+        const username = connectedUsers[message.id].username;
+
+        Object.keys(connectedUsers).forEach(userId => {
+          if (connectedUsers[userId].id !== message.id) {
+            const historyKey = `${senderId}-${connectedUsers[userId].id}`;
+            if (!messageHistory[historyKey]) {
+                messageHistory[historyKey] = [];
+            }
+            io.to(`user-${connectedUsers[userId].id}`).emit('message', { senderId, username, historyKey, message: formattedMessage });
+          }
+        });
+    }
+    else{
+        const senderId = connectedUsers[message.id].id;
         const receiverId = message.receiverId;
         const historyKey = `${senderId}-${receiverId}`;
         const timestamp = new Date().toLocaleTimeString();
-        const formattedMessage = `${timestamp} - ${connectedUsers[socket.id].username}: ${message.text}`;
+        const formattedMessage = `${timestamp} - ${connectedUsers[message.id].username}: ${message.text}`;
 
         if (!messageHistory[historyKey]) {
             messageHistory[historyKey] = [];
         }
+        const username = connectedUsers[message.id].username;
 
         messageHistory[historyKey].push(formattedMessage);
-        io.to(`user-${receiverId}`).emit('message', { historyKey, message: formattedMessage });
-    });
+        io.to(`user-${receiverId}`).emit('message', { senderId, username, historyKey, message: formattedMessage });
+    }
+  });
 
-    // Gestion du message "all"
-    socket.on('message-all', (message) => {
-        const senderId = connectedUsers[socket.id].id;
-        const timestamp = new Date().toLocaleTimeString();
-        const formattedMessage = `${timestamp} - ${connectedUsers[socket.id].username} à tous : ${message.text}`;
-    
-        // Diffuser le message à tous les utilisateurs connectés sauf à l'émetteur
-        Object.keys(connectedUsers).forEach(userId => {
-            if (userId !== socket.id) {
-                io.to(userId).emit('message', { historyKey: 'all', message: formattedMessage });
-            }
-        });
-    });
 
-    socket.on('disconnect', () => {
-        console.log('Un utilisateur s\'est déconnecté');
-        delete connectedUsers[socket.id];
-        io.emit('update-user-list', Object.values(connectedUsers));
-    });
+
+  socket.on('disconnect', () => {
+    console.log('Un utilisateur s\'est déconnecté');
+    delete connectedUsers[socket.id];
+    io.emit('update-user-list', Object.values(connectedUsers));
+  });
 });
 
 server.listen(4000, () => {
-    console.log('Le serveur est en écoute sur le port 4000');
+  console.log('Le serveur est en écoute sur le port 4000');
 });
--- frontend/react-front/src/chat-component/ChatComponent.js
+++ frontend/react-front/src/chat-component/ChatComponent.js
@@ -1,83 +1,137 @@
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/actions";
 import HeaderBox from "../utilities/header";
-import React, {useCallback, useEffect, useState} from "react";
-import {io} from "socket.io-client";
-import {useSelector} from "react-redux";
-import {selectUser} from "../store/actions";
-
 
 function ChatComponent() {
-    const socket = io('http://localhost:4000');
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
-    const [recipientList, setRecipientList] = useState([]);
-    const [selectedRecipient, setSelectedRecipient] = useState(null);
-    const [isUserConnected, setIsUserConnected] = useState(false);
-    const loggedUser = useSelector(selectUser);
+  const socket = io("http://localhost:4000");
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [recipientList, setRecipientList] = useState([]);
+  const [selectedRecipient, setSelectedRecipient] = useState(null);
+  const [isUserConnected, setIsUserConnected] = useState(false);
+  const loggedUser = useSelector(selectUser);
 
-    const handleSendMessage = () => {
-        const messageObject = {
-            sender: loggedUser.id,
-            receiver : selectedRecipient.id,
-            text: newMessage,
-        };
-        socket.emit('send-message', messageObject);
-        // TODO : add message to the list of messages
-        setMessages((messages) => [...messages, messageObject]);
-        setNewMessage('');
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("Connected to the chat server");
+      socket.emit("login", { username: loggedUser.username, password: loggedUser.password });
+    });
+
+    socket.on("login-success", (user) => {
+      console.log("Login successful", user);
+      setIsUserConnected(true);
+    });
+
+    return () => {
+      socket.disconnect();
     };
+  }, [loggedUser]);
 
+  useEffect(() => {
+    socket.on("update-user-list", (users) => {
+      updateRecipientList(users);
+    });
 
-    useEffect(() => {
-        socket.connect();
-        // Handle events within useEffect
-        socket.on('connect', () => {
-            console.log('Connected to the Chat Server');
-            socket.emit('login', { username: loggedUser.username, password: loggedUser.password });
-            socket.on('login-success', (user) => {
-                console.log('login-success', user);
-                setIsUserConnected(true);
-            });
-        });
+    socket.on("message", (data) => {
+      const { userid,username,historyKey, message } = data;
+      const timestamp = new Date().toLocaleTimeString();
 
-    }, [isUserConnected]);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        {
+          senderId: userid,
+          text: `${timestamp} - ${username}: ${message}`,
+        },
+      ]);
+    });
+  }, [selectedRecipient]);
 
-    useEffect(() => {
-        socket.on('update-user-list', (user) => {
-            console.log('update-user-list', user);
-            if (user.id !== loggedUser.id) {
-                setRecipientList((prevList) => [...prevList, user]);
-            }
-        });
-    }, [recipientList]);
-    return (
-        <div>
-            <HeaderBox title="Chat" />
-            <div>
-                <h3>Users</h3>
-                {recipientList.length !== 0 &&<ul>
-                    {recipientList.map((user) => (
-                        <li key={user.username} onClick={setSelectedRecipient(user)}>{user.username}</li>
-                    ))}
-                </ul>}
-            </div>
-            <div style={{ height: '300px', border: '1px solid #ccc', overflowY: 'scroll' }}>
-                {messages.map((message, index) => (
-                    <div key={index} style={{ padding: '8px', textAlign: message.sender === 'bot' ? 'left' : 'right' }}>
-                        <strong>{message.sender === 'bot' ? 'Bot:' : 'You:'}</strong> {message.text}
-                    </div>
-                ))}
-            </div>
-            <div style={{ marginTop: '10px' }}>
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    placeholder="Type your message..."
-                />
-                <button onClick={handleSendMessage}>Send</button>
-            </div>
-        </div>
-    );
-};
+  const handleSendMessage = () => {
+    const messageObject = {
+      id: loggedUser.id,
+      receiverId: selectedRecipient.id,
+      text: newMessage,
+    };
+    
+
+    socket.emit("message", messageObject);
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        senderId: loggedUser.id,
+        text: newMessage,
+      },
+    ]);
+
+    setNewMessage("");
+  };
+
+  const handleSendMessageAll = () => {
+    
+    const messageObject = {
+      id: loggedUser.id,
+      receiverId: -1,
+      text: newMessage,
+    };
+    
+
+    socket.emit("message", messageObject);
+
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        senderId: loggedUser.id,
+        text: newMessage,
+      },
+    ]);
+
+    setNewMessage("");
+  };
+
+  const updateRecipientList = (users) => {
+    setRecipientList(users.filter((user) => user.id !== loggedUser.id));
+  };
+
+  return (
+    <div>
+      <HeaderBox title="Chat" />
+      <div>
+        <h3>Utilisateurs</h3>
+        {recipientList.length !== 0 && (
+          <ul>
+            <li>Tout le monde</li>
+            {recipientList.map((user) => (
+              <li key={user.id} onClick={() => setSelectedRecipient(user)}>
+                {user.username}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div style={{ height: "300px", border: "1px solid #ccc", overflowY: "scroll" }}>
+        {messages.map((message, index) => (
+          <div key={index} style={{ padding: "8px", textAlign: "left" }}>
+            <strong>{message.senderId === loggedUser.id ? "Vous" : message.senderId}</strong>: {message.text}
+          </div>
+        ))}
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Tapez votre message..."
+        />
+        <button onClick={handleSendMessage}>Envoyer</button>
+        <button onClick={handleSendMessageAll}>Envoyer à tout le monde</button>
+      </div>
+    </div>
+  );
+}
 
 export default ChatComponent;
